f1e2d77bca98722383241f7b33653888cfe6d10a
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-17 05:04:26 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+  
+  def take(names, job)
+    @names = names 
+    @job = job
+  end
+  
+  def name= name 
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job= job
+    @job = job
+  end
+  
+  def job
+    @job
+  end 
+  
+end
